8a153056def56919b6a37576eabfcba3df7fd7bf
cgirard <camille.girard1995@gmail.com>
2024-07-07 15:08:22 +0200

    up header + footer

--- ESGI/footer.php
+++ ESGI/footer.php
@@ -1,7 +1,46 @@
 <footer id="site-footer">
-    <p>
-        © <?= date('Y') ?> Figma Template by ESGI
-    </p>
+    <div class="container">
+        <div class="container-footer">
+            <a href="/">
+                <?= esgi_get_icon('logo'); ?>
+            </a>
+            <div class="contact-footer">
+                <div class="contact-footer-item">
+                    <h5 class="contact-title">Manager</h5>
+
+                    <span></span>
+
+                    <span></span>
+                </div>
+                <div class="contact-footer-item">
+                    <h5 class="contact-title">CEO</h5>
+
+                    <span></span>
+
+                    <span></span>
+                </div>
+            </div>
+        </div>
+        <div class="container-footer-footer">
+            <div class="legal">
+                © <?= date('Y') ?> Figma Template by ESGI
+            </div>
+            <div class="rs">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <?= esgi_get_icon('linkedin'); ?>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <?= esgi_get_icon('facebook'); ?>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
 <?php wp_footer() ?>
 </body>
